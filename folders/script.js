765d555c06b40cce6acff5f0f038b651c637f601
Nathan Thomaz <114695899+NathanThomaz@users.noreply.github.com>
2024-03-14 16:30:21 -0300

    Update script

--- folders/script.js
+++ folders/script.js
@@ -1,6 +1,6 @@
 document.getElementById("btn_donwload_curriculo").addEventListener("click", function() {
     var link = document.createElement("a");
-    link.href = "/folders/arquivos/Curriculo_Nathan_Thomaz.pdf"; // Caminho do currículo
+    link.href = "./folders/arquivos/Curriculo_Nathan_Thomaz.pdf"; // Caminho do currículo
     link.download = "Curriculo_Nathan_Thomaz.pdf"; // Nome do arquivo
     link.click();
 });
