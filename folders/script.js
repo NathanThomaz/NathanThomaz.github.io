526539397151d786cd41972e9f6ea39c43fc0e71
Nathan Thomaz <114695899+NathanThomaz@users.noreply.github.com>
2024-03-14 16:13:49 -0300

    Update script.js

--- folders/script.js
+++ folders/script.js
@@ -2,7 +2,7 @@ document.getElementById("botao_curriculo").addEventListener("click", function() 
     // Criar um link temporário
     var link = document.createElement("a");
     // Adicionar atributos ao link
-    link.href = "./arquivos/Curriculo_Nathan_Thomaz.pdf"; // Substitua pelo caminho real do seu currículo
+    link.href = "./folders/arquivos/Curriculo_Nathan_Thomaz.pdf"; // Substitua pelo caminho real do seu currículo
     link.download = "Curriculo_Nathan_Thomaz.pdf"; // Nome do arquivo a ser baixado
     // Simular um clique no link
     link.click();
